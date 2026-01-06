3875f6fbf241ce504213d512242b87c2012db074
utkarsh14021999kumar <kumarutkarsh944@gmail.com>
2026-01-06 12:37:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask via Jenkins!"
+    return "Hello from Flassk via Jenkins!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
